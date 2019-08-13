89cd2a03323d252d66e7ea354f17347d3a4cf9ba
Henry Wong <H_wong14@u.pacific.edu>
2019-08-13 16:44:14 -0700

    Removed button, start using seekbar instead, percentage of seekbar displayed as changing

--- app/src/main/java/com/example/henry/tipapp/MainActivity.java
+++ app/src/main/java/com/example/henry/tipapp/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,12 +25,11 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
     InputMethodManager inputManager;
     EditText amountTextView;
-    Button bOne;
-    Button bTwo;
-    Button bThree;
+    SeekBar seekBar;
     TextView sumTextView;
     TextView personSumTextView;
     TextView totalPplTextView;
+    TextView percentProgressTextView;
     Spinner spinner;
     String[] numPeople = new String[20];
     String total;
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         double totalCalc;
         inputManager.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
         String s = amountTextView.getText().toString();
-        for (int i = 0; i < s.length(); i++){
+        for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == ',') {
                 Toast.makeText(this, "Invalid entry, please enter again", Toast.LENGTH_SHORT).show();
                 spinner.setEnabled(false);
@@ -47,10 +47,9 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        if (s.equals("")){
+        if (s.equals("")) {
             spinner.setEnabled(false);
         }
-
 
 
         String tip = ((Button) view).getText().toString();
@@ -59,16 +58,15 @@ public class MainActivity extends AppCompatActivity {
 
 
         double amounts = Double.parseDouble(amountTextView.getText().toString());
-        double numTip = Double.parseDouble(tipper)* (0.01);
+        double numTip = Double.parseDouble(tipper) * (0.01);
 
-        if (amountTextView.getText().equals("")){
+        if (amountTextView.getText().equals("")) {
             Toast.makeText(this, "Please enter the total amount before tipping", Toast.LENGTH_SHORT).show();
         } else {
 
 
-
             sumTextView.setVisibility(View.VISIBLE);
-            totalCalc = amounts + (numTip*amounts);
+            totalCalc = amounts + (numTip * amounts);
             totalCalc = Math.round(totalCalc * 100);
             totalCalc = totalCalc / 100;
 
@@ -77,8 +75,8 @@ public class MainActivity extends AppCompatActivity {
                 personSumTextView.setVisibility(View.INVISIBLE);
             } else {
                 Double numOfPeople = Double.parseDouble(spinner.getItemAtPosition(spinner.getSelectedItemPosition()).toString());
-                String total2 = String.format("%.2f" ,(totalCalc / numOfPeople));
-                personSumTextView.setText(total2);
+                String total2 = String.format("%.2f", (totalCalc / numOfPeople));
+                personSumTextView.setText("$" + total2);
             }
 
 
@@ -89,34 +87,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        percentProgressTextView = findViewById(R.id.percentProgressTextView);
         amountTextView = findViewById(R.id.amountTextView);
-        bOne = findViewById(R.id.button);
-        bTwo = findViewById(R.id.button2);
-        bThree = findViewById(R.id.button3);
+        seekBar = findViewById(R.id.seekBar);
         sumTextView = findViewById(R.id.sumTextView);
         personSumTextView = findViewById(R.id.personSumTextView);
         totalPplTextView = findViewById(R.id.textView4);
         inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 
+        seekBar.setMin(0);
+        seekBar.setMax(30);
+        seekBar.setProgress(15);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                percentProgressTextView.setText(Integer.toString(i));
+            }
 
-        bOne.setEnabled(false);
-        bTwo.setEnabled(false);
-        bThree.setEnabled(false);
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
 
+            }
 
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         spinner = findViewById(R.id.spinner);
 
 
         for (int i = 0; i < numPeople.length; i++) {
-            numPeople[i] = Integer.toString(i+1);
+            numPeople[i] = Integer.toString(i + 1);
         }
 
 
@@ -128,15 +136,15 @@ public class MainActivity extends AppCompatActivity {
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if (i+1 >= 2) {
+                if (i + 1 >= 2) {
                     totalPplTextView.setVisibility(View.VISIBLE);
                     personSumTextView.setVisibility(View.VISIBLE);
-                    double totalPerPerson = Double.parseDouble(total) / (i+1);
+                    double totalPerPerson = Double.parseDouble(total) / (i + 1);
 
-                    totalPerPerson = Math.round(totalPerPerson*100);
+                    totalPerPerson = Math.round(totalPerPerson * 100);
                     totalPerPerson = totalPerPerson / 100;
 
-                    String totalPP = String.format("%.2f",totalPerPerson);
+                    String totalPP = String.format("%.2f", totalPerPerson);
 
                     personSumTextView.setText("$" + totalPP);
                 }
@@ -154,94 +162,8 @@ public class MainActivity extends AppCompatActivity {
         amountTextView.setRawInputType(Configuration.KEYBOARD_QWERTY);
 
 
-        amountTextView.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if(amountTextView.toString().trim().length()==0){
-                    bOne.setEnabled(false);
-                    bTwo.setEnabled(false);
-                    bThree.setEnabled(false);
-                    spinner.setEnabled(false);
-                } else {
-                    bOne.setEnabled(true);
-                    bTwo.setEnabled(true);
-                    bThree.setEnabled(true);
-                    spinner.setEnabled(true);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
 
 
-        Intent returnIntent = getIntent();
-        if (returnIntent.getStringExtra("btnOne") != null) {
-            bOne.setText(returnIntent.getStringExtra("btnOne") + "%");
-
-        } else if (returnIntent.getStringExtra("btnTwo") != null) {
-            bTwo.setText(returnIntent.getStringExtra("btnTwo") + "%");
-
-        } else if (returnIntent.getStringExtra("btnThree")!= null) {
-            bThree.setText(returnIntent.getStringExtra("btnThree") + "%");
-
-        }
-
-
-        bOne.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                String fButtonText = bOne.getText().toString();
-                String[] part = fButtonText.split("%");
-                Double buttonNum = Double.parseDouble(part[0]);
-                Intent intent = new Intent(getApplicationContext(), ButtonEdit_Activity.class);
-                intent.putExtra("num1", buttonNum);
-                intent.putExtra("key","one");
-                Log.i("SENDING", "INFO");
-                startActivity(intent);
-
-                return true;
-            }
-        });
-
-        bTwo.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                String fButtonText = bTwo.getText().toString();
-                String[] part = fButtonText.split("%");
-                Double buttonNum = Double.parseDouble(part[0]);
-                Intent intent = new Intent(getApplicationContext(), ButtonEdit_Activity.class);
-                intent.putExtra("num2",buttonNum);
-                intent.putExtra("key","two");
-                Log.i("SENDING", "INFO");
-                startActivity(intent);
-                return true;
-            }
-        });
-
-        bThree.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                String fButtonText = bThree.getText().toString();
-                String[] part = fButtonText.split("%");
-                Double buttonNum = Double.parseDouble(part[0]);
-                Intent intent = new Intent(getApplicationContext(), ButtonEdit_Activity.class);
-                intent.putExtra("num3", buttonNum);
-                intent.putExtra("key","three");
-                Log.i("SENDING", "INFO");
-                startActivity(intent);
-
-                return true;
-            }
-        });
-        sumTextView.setVisibility(View.INVISIBLE);
 
     }
 }
